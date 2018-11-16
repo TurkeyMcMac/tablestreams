2d370d159a218d2e390b469d2a1ba3dd65bec6a8
TurkeyMcMac <jwmhjwmh@gmail.com>
2018-11-16 11:40:28 -0500

    Add some TSV reader tests

---
+++ src/tests/TSVTableReaderTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.StringReader;
+
+import org.junit.jupiter.api.Test;
+import jwmh.tablestreams.*;
+
+class TSVTableReaderTest
+{
+
+
+  @Test
+  void trailingLine() throws Exception
+  {
+    assertOutput("a\tb\tc\nd\te\tf\n","\"a\":\"b\":\"c\"\n\"d\":\"e\":\"f\"");
+  }
+  
+  @Test
+  void noTrailingLine() throws Exception
+  {
+    assertOutput("a\tb\tc\nd\te\tf","\"a\":\"b\":\"c\"\n\"d\":\"e\":\"f\"");
+  }
+
+  void assertOutput(String input, String output) throws Exception
+  {
+    StringReader source = new StringReader(input);
+    TableReader reader = new TSVTableReader(source);
+    OutputStreamSimulator dest = new OutputStreamSimulator(output);
+    TableWriter writer = new DSVTableWriter(dest, ':');
+    writer.writeTable(reader);
+    dest.close();
+  }
+
+}
